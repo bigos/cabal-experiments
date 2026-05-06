dde4d3eafa92a2c29ad32f703e1dd83a3c07c219
Jacek Podkanski <ruby.object@googlemail.com>
2026-05-06 18:09:30 +0100

    removed GraphID Str

--- graphviz-simple/app/Main.hs
+++ graphviz-simple/app/Main.hs
@@ -29,7 +29,7 @@ import Data.Text.Lazy.IO qualified as T
 
 graph :: DotGraph String
 graph =
-  digraph (Str "G" :: GraphID Str) $ do
+  digraph (Str "G") $ do
     cluster (Str "cluster0") $ do
       graphAttrs [style filled, color LightGray]
       nodeAttrs [style filled, color White]
