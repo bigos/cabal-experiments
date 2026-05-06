a38735bcf7a7a929d58c912bdab57ec8bddff261
Jacek Podkanski <Jacek.Podkanski@sgs.com>
2026-05-06 16:20:40 +0100

    some progress but not quite there yet

--- graphviz-simple/app/Main.hs
+++ graphviz-simple/app/Main.hs
@@ -3,40 +3,61 @@
 module Main where
 
 import Data.GraphViz
-import Data.GraphViz.Printing (renderDot)
+import Data.GraphViz.Attributes
+  ( color,
+    filled,
+    shape,
+    style,
+    textLabel,
+  )
+import Data.GraphViz.Attributes.Complete
+  ( Color (..),
+    Number (..),
+    Shape (..),
+    StyleItem (..),
+  )
+import Data.GraphViz.Printing (renderDot, toDot)
+import Data.GraphViz.Types.Monadic
+  ( GraphID (..),
+    cluster,
+    digraph,
+    graphAttrs,
+    node,
+    nodeAttrs,
+    (-->),
+  )
 import Data.Text.Lazy.IO qualified as T
 import Prelude hiding (Int)
 
 -- avoid clash with GraphViz Int
 
-graph :: b
+-- graph :: Data.GraphViz.Types.Generalised.DotGraph String
 graph =
-  digraph (Str "G") $
-    do
-      cluster (Int 0) $ do
-        graphAttrs [style filled, color LightGray]
-        nodeAttrs [style filled, color White]
-        "a0" --> "a1"
-        "a1" --> "a2"
-        "a2" --> "a3"
-        graphAttrs [textLabel "process #1"]
+  digraph (Str "G") $ do
+    cluster (Int 0) $ do
+      graphAttrs [style filled, color LightGray]
+      nodeAttrs [style filled, color White]
+      "a0" --> "a1"
+      "a1" --> "a2"
+      "a2" --> "a3"
+      graphAttrs [textLabel "process #1"]
 
-      cluster (Int 1) $ do
-        nodeAttrs [style filled]
-        "b0" --> "b1"
-        "b1" --> "b2"
-        "b2" --> "b3"
-        graphAttrs [textLabel "process #2", color Blue]
+    cluster (Int 1) $ do
+      nodeAttrs [style filled]
+      "b0" --> "b1"
+      "b1" --> "b2"
+      "b2" --> "b3"
+      graphAttrs [textLabel "process #2", color Blue]
 
-      "start" --> "a0"
-      "start" --> "b0"
-      "a1" --> "b3"
-      "b2" --> "a3"
-      "a3" --> "end"
-      "b3" --> "end"
+    "start" --> "a0"
+    "start" --> "b0"
+    "a1" --> "b3"
+    "b2" --> "a3"
+    "a3" --> "end"
+    "b3" --> "end"
 
-      node "start" [shape MDiamond]
-      node "end" [shape MSquare]
+    node "start" [shape MDiamond]
+    node "end" [shape MSquare]
 
 main :: IO ()
 main = do
