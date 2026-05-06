04175b416ac2566d064fb32509bafc9aab864a31
Jacek Podkanski <Jacek.Podkanski@sgs.com>
2026-05-06 15:32:10 +0100

    the AI advice was not that good

--- graphviz-simple/app/Main.hs
+++ graphviz-simple/app/Main.hs
@@ -2,8 +2,6 @@
 
 module Main where
 
--- avoid clash with GraphViz Int
-
 import Data.GraphViz
 import Data.GraphViz.Attributes
   ( color,
@@ -18,6 +16,7 @@ import Data.GraphViz.Attributes.Complete
     Shape (..),
     StyleItem (..),
   )
+import Data.GraphViz.Printing (renderDot, toDot)
 import Data.GraphViz.Types.Monadic
   ( GraphID (Str),
     cluster,
@@ -27,36 +26,42 @@ import Data.GraphViz.Types.Monadic
     nodeAttrs,
     (-->),
   )
+import Data.Text.Lazy.IO qualified as T
 import Prelude hiding (Int)
 
+-- avoid clash with GraphViz Int
+
 graph =
-  digraph (Str "G") $ do
-    cluster (Int 0) $ do
-      graphAttrs [style filled, color LightGray]
-      nodeAttrs [style filled, color White]
-      "a0" --> "a1"
-      "a1" --> "a2"
-      "a2" --> "a3"
-      graphAttrs [textLabel "process #1"]
+  digraph (Str "G") $
+    do
+      cluster (Int 0) $ do
+        graphAttrs [style filled, color LightGray]
+        nodeAttrs [style filled, color White]
+        "a0" --> "a1"
+        "a1" --> "a2"
+        "a2" --> "a3"
+        graphAttrs [textLabel "process #1"]
 
-    cluster (Int 1) $ do
-      nodeAttrs [style filled]
-      "b0" --> "b1"
-      "b1" --> "b2"
-      "b2" --> "b3"
-      graphAttrs [textLabel "process #2", color Blue]
+      cluster (Int 1) $ do
+        nodeAttrs [style filled]
+        "b0" --> "b1"
+        "b1" --> "b2"
+        "b2" --> "b3"
+        graphAttrs [textLabel "process #2", color Blue]
 
-    "start" --> "a0"
-    "start" --> "b0"
-    "a1" --> "b3"
-    "b2" --> "a3"
-    "a3" --> "end"
-    "b3" --> "end"
+      "start" --> "a0"
+      "start" --> "b0"
+      "a1" --> "b3"
+      "b2" --> "a3"
+      "a3" --> "end"
+      "b3" --> "end"
 
-    node "start" [shape MDiamond]
-    node "end" [shape MSquare]
+      node "start" [shape MDiamond]
+      node "end" [shape MSquare]
 
+main :: IO ()
 main = do
+  T.writeFile "/tmp/graph.dot" (renderDot (toDot graph))
   putStrLn "Tell me your name"
   name <- getLine
   putStrLn ("Hello " ++ name ++ ", nice to meet you.")
