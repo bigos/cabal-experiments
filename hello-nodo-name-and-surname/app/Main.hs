6cfb8a811205286e4bde1bbf4c243c8662b863fb
Jacek Podkanski <ruby.object@googlemail.com>
2026-05-11 12:44:01 +0100

    managed to get indentation right

--- hello-nodo-name-and-surname/app/Main.hs
+++ hello-nodo-name-and-surname/app/Main.hs
@@ -1,9 +1,10 @@
 module Main where
 
 main :: IO ()
-main =  putStrLn "Hello user! Who are you?" >>
-  putStrLn "tell me your first name" >>
-  getLine >>= \firstname ->
-  putStrLn "tell me your surname" >>
-  getLine >>= \surname ->
-  putStrLn ("Oh, nice to meet you Mr " ++ firstname ++ " " ++ surname)
+main =
+  putStrLn "Hello user! Who are you?"
+    >> putStrLn "tell me your first name"
+    >> getLine >>= \firstname ->
+      putStrLn "tell me your surname"
+        >> getLine >>= \surname ->
+          putStrLn ("Oh, nice to meet you Mr " ++ firstname ++ " " ++ surname)
