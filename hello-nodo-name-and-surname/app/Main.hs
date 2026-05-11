d02978fa7fccf7ef2244bcaff866f5f31782fb8c
Jacek Podkanski <Jacek.Podkanski@sgs.com>
2026-05-11 13:03:33 +0100

    indentation works after ghcup tui to install and set language  server

--- hello-nodo-name-and-surname/app/Main.hs
+++ hello-nodo-name-and-surname/app/Main.hs
@@ -1,10 +1,17 @@
 module Main where
 
 main :: IO ()
-main   =
-  putStrLn "Hello user! Who are you?"  >>
-  putStrLn "tell me your first name"    >>
-  getLine >>= \firstname ->
-  putStrLn                "tell me your surname"        >>
-  getLine >>= \surname ->
-  putStrLn ("Oh, nice to meet you Mr " ++ firstname ++ " " ++ surname)
+main =
+  putStrLn "Hello user! Who are you?"
+    >> putStrLn "tell me your first name"
+    >> getLine
+    >>= \firstname ->
+      putStrLn "tell me your surname"
+        >> getLine
+        >>= \surname ->
+          putStrLn
+            ( "Oh, nice to meet you Mr "
+                ++ firstname
+                ++ " "
+                ++ surname
+            )
